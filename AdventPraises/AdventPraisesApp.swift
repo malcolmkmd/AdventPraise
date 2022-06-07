eb1b152d27f6c5aa203a7086905e9935580deee5
malcolm <malcolm@luno.com>
2022-06-07 19:18:42 +0200

    Initial Commit

---
+++ AdventPraises/AdventPraisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventPraisesApp.swift
+//  AdventPraises
+//
+//  Created by Malcolm on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct AdventPraisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
